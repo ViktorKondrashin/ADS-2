05abb19a7e787e353e55f32ea6abc978ee4de10f
ViktorKondrashin <visktor.srgeev@mail.ru>
2025-03-11 20:05:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,26 +3,55 @@
 #include "alg.h"
 
 
-double pown(double value, uint16_t n) {
-  return 0.0;
+double pown(double value, int n) {
+  double rez = 1.0;
+  for (int i = 0; i < n; ++i) {
+    rez *= value;
+  }
+  return rez;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t rez = 1;
+  for (size_t i = 2; i <= n; ++i) {
+    rez *= i;
+  }
+  return rez;
 }
 
-double calcItem(double x, uint16_t n) {
-  return 0.0;
+double calc_item(double x, uint16_t n) {
+  long double rez = pown(x, n) / fact(n);
+  return rez;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  long double rez = 1.0L;
+  for (size_t i = 1; i <= count; ++i) {
+    rez += pown(x, i) / fact(i);
+  }
+  return rez;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double rez = 0.0;
+  for (size_t i = 1; i <= count; ++i) {
+    if (i % 2 == 0) {
+      rez -= pown(x, 2 * i - 1) / fact(2 * i - 1);
+    } else {
+      rez += pown(x, 2 * i - 1) / fact(2 * i - 1);
+    }
+  }
+  return rez;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double rez = 0.0;
+  for (size_t i = 1; i <= count; ++i) {
+    if (i % 2 == 0) {
+      rez -= pown(x, 2 * i - 2) / fact(2 * i - 2);
+    } else {
+      rez += pown(x, 2 * i - 2) / fact(2 * i - 2);
+    }
+  }
+  return rez;
 }
